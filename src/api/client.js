30a2829238a2f4ac88866b40aa81f2ee27148a34
xuannhat1503 <nguyenxuannhat15324@gmail.com>
2025-12-08 17:15:10 +0700

    update

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://optibuildhub-backend.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 export const api = axios.create({
   baseURL: API_URL,
